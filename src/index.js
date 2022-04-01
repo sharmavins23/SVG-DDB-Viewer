5ab589475f4bd1d822823b2e876b68836bc12c5a
sharmavins23 <sharmavins23@gmail.com>
2022-04-01 17:23:44 -0400

    added base files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // Components
+import TitleSegment from "./components/TitleSegment";
+import SVGSelector from "./components/SVGSelector";
+import Viewer from "./components/Viewer";
+import SVGUploadForm from "./components/SVGUploadForm";
 // Styles
-import "./index.css";
+import "./index.css"; // Compiler will kill me if I remove this
 
 ReactDOM.render(
     <React.StrictMode>
-        <h1>This works!</h1>
+        <TitleSegment />
+        <SVGSelector />
+        <Viewer />
+        <SVGUploadForm />
     </React.StrictMode>,
     document.getElementById("root")
 );
