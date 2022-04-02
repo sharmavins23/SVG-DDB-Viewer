bd54d55cdfca982c244b9cb4b18411074ce3d2a5
sharmavins23 <sharmavins23@gmail.com>
2022-04-02 11:57:22 -0400

    why does this project Just Work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Imports
 import React, { useState } from "react";
 import { Divider } from "antd";
+import GUN from "gun";
 // Components
 import TitleSegment from "./components/TitleSegment";
 import Viewer from "./components/Viewer";
@@ -8,6 +9,9 @@ import SVGUploadForm from "./components/SVGUploadForm";
 
 export default function App() {
     // Contains all individual components
+
+    // Initialize gun
+    const gun = GUN();
 
     // States
     // Current SVG displayed
@@ -41,13 +45,31 @@ export default function App() {
 
     // Callback function to update the SVG within a child component
     const updateSVG = (newSVG) => {
-        setSVG(newSVG); // For now, just change the SVG on the page
+        // Update the GUN database with the new SVG
+        gun.get(newSVG.title).put({
+            title: newSVG.title,
+            svg: newSVG.svg,
+        });
+
+        // Update the SVG name list
+        setSVGNameList(svgNameList.concat(newSVG.title));
     };
 
     // Callback function to update the SVG from the selector
     const changeCurrentSVG = (selectionTitle) => {
-        console.log(selectionTitle);
-        setSVGNameList([selectionTitle]);
+        // Ensure that the previously selected SVG is pushed to GUN
+        gun.get(svg.title).put({
+            title: svg.title,
+            svg: svg.svg,
+        });
+
+        // Get the new SVG from GUN and update the state
+        gun.get(selectionTitle).on((data, key) => {
+            setSVG({
+                title: data.title,
+                svg: data.svg,
+            });
+        });
     };
 
     return (
